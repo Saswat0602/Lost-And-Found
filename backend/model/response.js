84cd19a081453e24b81529bb93e22395790efc3b
Sawat <saswatranjan0602@gmail.com>
2024-03-23 13:20:56 +0530

    added response model

---
+++ backend/model/response.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const responseSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  item: {
+    type: Schema.Types.ObjectId,
+    ref: "Item",
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  confirmation: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Response = mongoose.model("Response", responseSchema);
+
+export default Response;
